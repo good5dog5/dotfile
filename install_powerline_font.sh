cfba244d6af694fe3695466d945958df5a7370d4
Jordan <good5dog5@gmail.com>
2015-01-08 01:28:00 +0800

    Update dotfile

--- install_powerline_font.sh
+++ install_powerline_font.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+printf "%50s\n\n" "=>Begin Downloading Powerline font and update font cache"
+
 dir=~/.fonts/
 
 
 cp ./bin/Droid Sans Mono for Powerline.otf $dir
 fc-cache -vf $dir && printf "%-55s %5s\n" "Update fonts cache\n\n" "[done]"
-
-
 printf "%50s\n" "finish"
 
 
