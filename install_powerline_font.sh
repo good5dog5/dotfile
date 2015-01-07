3dd985dc890fba370ad25d82d590f46196ddca20
jordan huang <good5dog5@gmail.com>
2015-01-07 17:21:44 +0800

    Modify for powerline patched font

--- install_powerline_font.sh
+++ install_powerline_font.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-printf "%50s\n\n" "=>Begin Downloading Powerline font and update font cache"
+
+dir=~/.fonts/
 
 
-wget -q -P $dir   https://github.com/powerline/fonts/blob/master/DroidSansMono/Droid%20Sans%20Mono%20for%20Powerline.otf && printf "%-55s %5s\n" "Copy DroidSansMono.oft to $dir" "[done]"
-fc-cache -vf && printf "%-55s %5s\n" "Update fonts cache\n\n" "[done]"
+cp ./bin/Droid Sans Mono for Powerline.otf $dir
+fc-cache -vf $dir && printf "%-55s %5s\n" "Update fonts cache\n\n" "[done]"
 
 
 printf "%50s\n" "finish"
