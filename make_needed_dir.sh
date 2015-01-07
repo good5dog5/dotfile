d11017dbd20dc4acb2a21dcea92c8c89874bcf56
jordan huang <good5dog5@gmail.com>
2015-01-07 14:56:49 +0800

    Update

--- make_needed_dir.sh
+++ make_needed_dir.sh
@@ -3,11 +3,11 @@
 vim_dir=~/.vim/colors/
 font_dir=~/.fonts/
 
-if [! -d "$vim_dir"]; then
+if [ ! -d "$vim_dir" ]; then
    mkdir -p $vim_dir && printf "%-35s %6s\n" "Make dir $vim_dir" "[done]"
 fi
 
-if [! -d "$font_dir"]; then
+if [ ! -d "$font_dir" ]; then
    mkdir $font_dir && printf "%-35s %6s\n" "Make dir $font_dir" "[done]"
 fi
 
