16c03fe4e1428b961dc6fe19f1cdba3eb88ffc2a
jordan huang <good5dog5@gmail.com>
2015-01-12 16:54:45 +0800

    Update dotfile and add new vimperator config file vimperatorrc

--- install_Devtools.sh
+++ install_Devtools.sh
@@ -5,3 +5,8 @@ sudo ln -fs /usr/bin/colorgcc  gcc
 sudo ln -fs /usr/bin/colorgcc  g++
 sudo ln -fs /usr/bin/colorgcc  cc
 sudo ln -fs /usr/bin/colorgcc  c++
+
+cd ~/usr
+git clone https://github.com/pagekite/Colormake
+cd Colormake
+sudo cp -fa colormake.pl colormake colormake-short clmake clmake-short /usr/bin/
