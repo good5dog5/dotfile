8e652473bbfa3947da3d15fe892305a879a42078
Jordan Huang <good5dog5@gmail.com>
2015-03-20 11:58:43 +0800

    [script][fix] Fix cscope_gen_databash.sh syntax issue.
    Former-commit-id: 92ee13ce232ec254df9f279a5bd0443dc0b002d9

--- script/cscope_gen_database.sh
+++ script/cscope_gen_database.sh
@@ -1,6 +1,6 @@
-DIR[0] = "./"
-DIR[1] = "/usr/include/i386-linux-gnu/sys/"
-DIR[2] = "/usr/include/i386-linux-gnu/bits/"
+DIR[0]="./"
+DIR[1]="/usr/include/i386-linux-gnu/sys/"
+DIR[2]="/usr/include/i386-linux-gnu/bits/"
 
 find /usr/include/ -maxdepth 1 -type f -name "*.[hc]" >> cscope.files
 
