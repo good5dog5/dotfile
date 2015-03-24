96c2f33b8649be753ba630c6d3905554cca8979a
Jordan Huang <good5dog5@gmail.com>
2015-03-24 16:20:13 +0800

    [script] Modify make_needed_dir.sh with new directory.

--- setup/make_needed_dir.sh
+++ setup/make_needed_dir.sh
@@ -4,11 +4,11 @@ DIR[0]="$HOME/.vim/colors/"
 DIR[1]="$HOME/.fonts/"
 DIR[2]="$HOME/usr/script/"
 DIR[3]="$HOME/stardict/dic/"
-
+DIR[4]="/tftpboot/scripts/"
 
 for d in "${DIR[@]}"; do
     if [ ! -d "$d" ]; then
-       mkdir -p $d && printf "%-35s %6s\n" "Make dir $d " "[done]"
+       sudo mkdir -p $d && printf "%-35s %6s\n" "Make dir $d " "[done]"
     fi
 done
 
