34589d4bd0f49e32b7fe05f412d42fa6379e58a6
Jordan Huang <good5dog5@gmail.com>
2015-04-11 12:41:31 +0800

    Update setup script for permission problem.

--- setup/install_autojump.sh
+++ setup/install_autojump.sh
@@ -1,8 +1,12 @@
+#!/bin/sh
 DEST_FOLDER=~/usr
 
-git clone https://github.com/joelthelion/autojump.git $DEST_FOLDER/autojump
-cd $DEST_FOLDER/autojump
+if [ ! -d $DEST_FOLDER ];
+then
+	git clone https://github.com/joelthelion/autojump.git $DEST_FOLDER/autojump
+	cd $DEST_FOLDER/autojump
 
-chmod +x install.py 
-./install.py
+	chmod +x install.py 
+	./install.py
+fi
 
--- setup/install_vundle.sh
+++ setup/install_vundle.sh
@@ -1,1 +1,5 @@
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/
+#/bin/sh
+if [ ! -d ~/.vim/bundle/Vundle.vim ];
+then
+	 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
