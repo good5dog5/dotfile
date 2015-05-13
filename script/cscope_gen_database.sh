6d871f0d896aca607652d37a849d76d77ac3a7e6
Jordan Huang <good5dog5@gmail.com>
2015-05-13 11:49:16 +0800

    Update dotfiles
    Former-commit-id: e081146b8e8a8d7d9686829847c6108db6167a73

--- script/cscope_gen_database.sh
+++ script/cscope_gen_database.sh
@@ -2,6 +2,8 @@ DIR[0]="./"
 DIR[1]="/usr/include/i386-linux-gnu/sys/"
 DIR[2]="/usr/include/i386-linux-gnu/bits/"
 
+[ ! -d $cscope_dir ] && mkdir $cscope_dir
+
 find /usr/include/ -maxdepth 1 -type f -name "*.[hc]" >> cscope.files
 
 for d in "${DIR[@]}"; do
