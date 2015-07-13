b3ea5b877945ee714e34126e3bc5770d71a47486
Jordan Huang <good5dog5@gmail.com>
2015-07-13 15:47:15 +0800

    Add script to buid LINE for linux (support by pidgin).

---
+++ setup/build_line_pidgin.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+build_Apache_Thrift()
+{
+    sudo apt-get install -y         \
+        libboost-dev                \
+        libboost-test-dev           \
+        libboost-program-options-dev\
+        libboost-system-dev         \
+        libboost-filesystem-dev     \
+        libevent-dev                \
+        libpurple-dev                \
+        automake                    \
+        libtool                     \
+        flex                        \
+        bison                       \
+        pkg-config                  \
+        g++                         \
+        libssl-dev                  
+
+    cd /tmp
+    curl http://ftp.twaren.net/Unix/Web/apache/thrift/0.9.2/thrift-0.9.2.tar.gz | tar zx
+     cd thrift-0.9.2/ 
+     ./configure 
+    make -j16 
+    sudo checkinstall
+    thrift --help 
+}
+build_line()
+{
+    cd /tmp
+    git clone http://altrepo.eu/git/purple-line.git/ purple-line
+    git clone http://altrepo.eu/git/line-protocol.git/ line-protocol
+    cp line-protocol/line_main.thrift purple-line/
+    cp line-protocol/line.thrift purple-line/
+
+    cd purple-line
+    make
+
+    sudo make install
+}
+
+build_Apache_Thrift
+build_line
