0d5fd347ed4aa8f8f36d86c1bb0ac9295d784da1
Jordan Huang <good5dog5@gmail.com>
2015-11-22 23:54:24 +0800

    [bash] Update bash configs.
    Former-commit-id: 45937f23b6906c4601e13628b24c19fefa82ec42 [formerly 2eda3649df75c25d579240b1658adf1be94b4c62]
    Former-commit-id: 140ce6604b702a2d2f9797bea4612890ac6561a9

--- bash/bash_alias/alias.dev.bash
+++ bash/bash_alias/alias.dev.bash
@@ -83,8 +83,10 @@ function Gcgi() {
 function x()
 {
     if [[ "$1" =~ .*.cpp ]]; then
-        g++ -std=c++11 -g -o ${1%.*}{.out,.${1##*.}} $2 $3 $4 $5  && time ./${1%.*}.out# && valgrind  ./${1%.*}.out
+        g++ -std=c++11 -g -o ${1%.*}{.out,.${1##*.}} $2 $3 $4 $5  && time ./${1%.*}.out
+        # && valgrind  ./${1%.*}.out
     else
-        gcc -std=c99 -g -o ${1%.*}{.out,.${1##*.}} $2 $3 $4 $5 -lm && time ./${1%.*}.out# && valgrind  ./${1%.*}.out
+        gcc -std=c99 -g -o ${1%.*}{.out,.${1##*.}} $2 $3 $4 $5 -lm && time ./${1%.*}.out
+        # && valgrind  ./${1%.*}.out
     fi
 }
