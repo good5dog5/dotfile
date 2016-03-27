62ec7403cffe9978ee2a208cf7d7c7d9958d434f
Jordan Huang <good5dog5@gmail.com>
2016-03-27 20:31:13 +0800

    Add todo.txt, a command line task manager.

---
+++ startup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Jordan huang<good5dog5@gmail.com>
+
+
+# https://wiki.archlinux.org/index.php/Keyboard_configuration_in_Xorg#Adjusting_typematic_delay_and_rate
+xset r rate 180 35
+synclient TouchpadOff=1
