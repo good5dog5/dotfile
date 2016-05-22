f3327c59bdde4d8f73c2ddf0b0805bf49e7757e1
Jordan Huang <good5dog5@gmail.com>
2016-05-22 10:14:28 -0400

    Update PPA.sh.
    Former-commit-id: 9bbda8c05fcc341395607bf6a6c32fa84b5a510d [formerly 4e9dd2e00c890d6dee0f261a2e930d5dbb0c0d89]
    Former-commit-id: fc3b84529236103296d9edc0c7be7ec2af057f39

---
+++ PKG/ubuntu/PPA.sh
@@ -0,0 +1,32 @@
+sudo apt-add-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-add-repository ppa:ubuntu-mozilla-daily/ppa -y
+sudo apt-add-repository ppa:banshee-team/ppa -y
+sudo apt-add-repository ppa:otto-kesselgulasch/gimp -y
+sudo apt-add-repository ppa:neovim-ppa/unstable -y
+sudo apt-add-repository ppa:libreoffice/ppa -y
+sudo apt-add-repository ppa:webupd8team/java -y
+sudo apt-add-repository ppa:xubuntu-dev/extras -y
+sudo apt-add-repository ppa:webupd8team/experiments -y
+sudo apt-add-repository ppa:pi-rho/dev -y
+sudo apt-add-repository ppa:oibaf/graphics-drivers -y
+sudo apt-add-repository ppa:relan/exfat -y
+sudo apt-add-repository 'http://mega.nz/linux/MEGAsync/xUbuntu_14.04/ ./' -y
+sudo apt-add-repository 'http://archive.canonical.com/ubuntu/ trusty partner' -y
+sudo apt-add-repository 'http://au.archive.ubuntu.com/ubuntu/ trusty main restricted' -y
+sudo apt-add-repository 'http://au.archive.ubuntu.com/ubuntu/ trusty-updates main restricted' -y
+sudo apt-add-repository 'http://au.archive.ubuntu.com/ubuntu/ trusty universe' -y
+sudo apt-add-repository 'http://au.archive.ubuntu.com/ubuntu/ trusty-updates universe' -y
+sudo apt-add-repository 'http://au.archive.ubuntu.com/ubuntu/ trusty multiverse' -y
+sudo apt-add-repository 'http://au.archive.ubuntu.com/ubuntu/ trusty-updates multiverse' -y
+sudo apt-add-repository 'http://au.archive.ubuntu.com/ubuntu/ trusty-backports main restricted universe multiverse' -y
+sudo apt-add-repository 'http://security.ubuntu.com/ubuntu trusty-security main restricted' -y
+sudo apt-add-repository 'http://security.ubuntu.com/ubuntu trusty-security universe' -y
+sudo apt-add-repository 'http://security.ubuntu.com/ubuntu trusty-security multiverse' -y
+sudo apt-add-repository 'http://extras.ubuntu.com/ubuntu trusty main' -y
+sudo apt-add-repository 'http://us.archive.ubuntu.com/ubuntu/ trusty main restricted' -y
+sudo apt-add-repository 'http://us.archive.ubuntu.com/ubuntu/ trusty-updates main restricted' -y
+sudo apt-add-repository 'http://us.archive.ubuntu.com/ubuntu/ trusty universe' -y
+sudo apt-add-repository 'http://us.archive.ubuntu.com/ubuntu/ trusty-updates universe' -y
+sudo apt-add-repository 'http://us.archive.ubuntu.com/ubuntu/ trusty multiverse' -y
+sudo apt-add-repository 'http://us.archive.ubuntu.com/ubuntu/ trusty-updates multiverse' -y
+sudo apt-add-repository 'http://us.archive.ubuntu.com/ubuntu/ trusty-backports main restricted universe multiverse' -y
