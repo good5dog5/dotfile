a424653671d575fc2201a3e43cca1e02ac46b476
Jordan Huang <good5dog5@gmail.com>
2017-09-06 13:15:09 +0800

    [i3] Add script to easier screenshot-> rename-> edit wiki's workflow.

---
+++ config/XDG_CONFIG/i3/customize_shot.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Jordan huang<good5dog5@gmail.com>
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+## 1. Take screenshot
+## 2. rename it 
+## 3. copy to wiki direct
+## 4. copy the path to clipboard
+
+wiki_pic_dir="$HOME/Dropbox/mywiki/static/img/postImg"
+pic_name=$(date +%F-%T).png
+
+scrot -s ~/Pictures/screenshot/"$pic_name"
+
+new_name=$(zenity --entry --text "Name the pic" --entry-text "$pic_name");
+cp ~/Pictures/screenshot/"$pic_name" "$wiki_pic_dir/$new_name"
+echo "/img/postImg/$new_name" | xclip -sel clip
