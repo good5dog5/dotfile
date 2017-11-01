5f9ae48d1e699802a60c7b2db0dee0c150eda6e3
Jordan Huang <good5dog5@gmail.com>
2017-11-01 20:15:09 +0800

    Update alias.
    Former-commit-id: af240235c3dd0e1d2d0ecc1f304e0cd095c583aa [formerly 6d9f333cee274a2a53f4c502cd28f000c7aaacfe]
    Former-commit-id: 8d1c2ac76df352060b494df9e94071c0b9fc0f4d

--- bash_utils/alias.common.bash
+++ bash_utils/alias.common.bash
@@ -147,9 +147,9 @@ F()
 }
 eba() {
    if [ "$#" -eq 0 ]; then
-       vim $HOME/bash_conf/alias.common.bash && sbr
+       vim $HOME/bash_utils/alias.common.bash && sbr
    else
-       vim $HOME/bash_conf/alias.$1.bash && sbr
+       vim $HOME/bash_utils/alias.$1.bash && sbr
    fi
 }
 md () { mkdir -p "$1"; }
@@ -482,3 +482,49 @@ vcsv()
 {
     csvtool readable "$@" | view -
 }
+
+# Docker
+# Get latest container ID
+alias dl="docker ps -l -q"
+
+# Get container process
+alias dps="docker ps"
+
+# Get process included stop container
+alias dpa="docker ps -a"
+
+# Get images
+alias di="docker images"
+
+# Get container IP
+alias dip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
+
+# Run deamonized container, e.g., $dkd base /bin/echo hello
+alias dkd="docker run -d -P"
+
+# Run interactive container, e.g., $dki base /bin/bash
+alias dki="docker run -i -t -P"
+
+# Execute interactive container, e.g., $dex base /bin/bash
+alias dex="docker exec -i -t"
+
+# Stop all containers
+dstop() { docker stop $(docker ps -a -q); }
+
+# Remove all containers
+drm() { docker rm $(docker ps -a -q); }
+
+# Stop and Remove all containers
+alias drmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
+
+# Remove all images
+dri() { docker rmi $(docker images -q); }
+
+# Dockerfile build, e.g., $dbu tcnksm/test 
+dbu() { docker build -t=$1 .; }
+
+# Show all alias related docker
+dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | sort; }
+
+# Bash into running container
+dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
