0043aab337a1401804e09e8f1521c8cfbd924f21
Jordan Huang <good5dog5@gmail.com>
2017-11-04 23:16:49 +0800

    Update pkg lists.

--- ubuntu-post-install/ubuntu-post-install-script.sh
+++ ubuntu-post-install/ubuntu-post-install-script.sh
@@ -5,15 +5,15 @@
 #   Sam Hewitt <sam@snwh.org>
 #
 # Description:
-#   A post-installation bash script for Ubuntu
+#   A set of post-installation script for Ubuntu and Ubuntu-based distributions
 #
 # Legal Preamble:
 #
-# This script is free software; you can redistribute it and/or modify it under
+# This project is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
 # Foundation; version 3.
 #
-# This script is distributed in the hope that it will be useful, but WITHOUT
+# This project is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
 # details.
@@ -25,9 +25,9 @@
 tabs 4
 clear
 
-#----- Import Functions -----#
+# Import functions
 
-dir="$(dirname "$0")"
+dir=$(dirname "$0")
 
 . $dir/functions/check
 . $dir/functions/cleanup
@@ -35,78 +35,98 @@ dir="$(dirname "$0")"
 . $dir/functions/configure
 . $dir/functions/development
 . $dir/functions/favs
+. $dir/functions/fonts
 . $dir/functions/gnome
+. $dir/functions/password
 . $dir/functions/thirdparty
 . $dir/functions/update
 . $dir/functions/utilities
 
-#----- Fancy Messages -----#
-show_error(){
-    echo -e "\033[1;31m$@\033[m" 1>&2
+
+# Fancy colorful messages
+
+# grey titles
+display_title(){
+echo -e "\033[1;30m$@\033[0m"
 }
-show_info(){
-    echo -e "\033[1;32m$@\033[0m"
+# red error messages
+display_error(){
+echo -e "\033[1;31m$@\033[0m" 1>&2
 }
-show_warning(){
-    echo -e "\033[1;33m$@\033[0m"
+# green info messages
+display_info(){
+echo -e "\033[1;32m$@\033[0m"
 }
-show_question(){
-    echo -e "\033[1;34m$@\033[0m"
+# yellow warning messages
+display_warning(){
+echo -e "\033[1;33m$@\033[0m"
 }
-show_success(){
-    echo -e "\033[1;35m$@\033[0m"
+# blue questions
+display_question(){
+echo -e "\033[1;34m$@\033[0m"
 }
-show_header(){
-    echo -e "\033[1;36m$@\033[0m"
+# pink success messages
+display_success(){
+echo -e "\033[1;35m$@\033[0m"
 }
-show_listitem(){
-    echo -e "\033[0;37m$@\033[0m"
+# cyan header messages
+display_header(){
+echo -e "\033[1;36m$@\033[0m"
 }
 
 # Main
 function main {
-eval `resize`
-MAIN=$(whiptail \
-    --notags \
-    --title "Ubuntu Post-Install Script" \
-    --menu "\nWhat would you like to do?" \
-    --cancel-button "Quit" \
-    $LINES $COLUMNS $(( $LINES - 12 )) \
-    update      'Perform system update' \
-    favs        'Install preferred applications' \
-    utilities   'Install preferred system utilities' \
-    development 'Install preferred development tools' \
-    codecs      'Install Ubuntu Restricted Extras' \
-    thirdparty  'Install third-party applications' \
-    gnome       'Install latest GNOME software' \
-    configure   'Configure system' \
-    cleanup     'Cleanup the system' \
-    3>&1 1>&2 2>&3)
+	display_title "Starting 'main' function..."
+	# Draw window
+	MAIN=$(eval `resize` && whiptail \
+		--notags \
+		--title "Ubuntu Post-Install Script" \
+		--menu "\nWhat would you like to do?" \
+		--cancel-button "Quit" \
+		$LINES $COLUMNS $(( $LINES - 12 )) \
+		update		'Perform system update' \
+		favs		'Install preferred applications' \
+		utilities	'Install preferred system utilities' \
+		development	'Install preferred development tools' \
+		codecs		'Install non-free codecs' \
+		fonts		'Install additional fonts' \
+		thirdparty	'Install third-party applications' \
+		gnome		'Install latest GNOME software' \
+		configure	'Configure system' \
+		cleanup		'Cleanup the system' \
+		3>&1 1>&2 2>&3)
 
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    $MAIN
-else
-    quit
-fi
+	# check exit status
+	EXITSTATUS=$?
+	if [ $EXITSTATUS = 0 ]; then
+		$MAIN
+	else
+		quit
+	fi
 }
 
 # Quit
 function quit {
-if (whiptail --title "Quit" --yesno "Are you sure you want quit?" 10 60) then
-    echo "Exiting..."
-    show_info 'Thanks for using!'
-    exit 99
-else
-    main
-fi
+	display_title "Starting 'quit' function..."
+	if (whiptail --title "Quit" --yesno "Are you sure you want quit?" 8 64) then
+		echo "Exiting..."
+		display_success 'Thanks for using!'
+		exit 99
+	else
+		main
+	fi
 }
 
-#RUN
-check_dependencies
+# Welcome message
+display_header "Ubuntu Post-Install Script"
+# Check
+display_title "Starting 'check' function..."
+check
+
+# Main
 while :
 do
-    main
+	main
 done
 
 #END OF SCRIPT
