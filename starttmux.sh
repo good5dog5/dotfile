7e76f41d922f2041e4dd085c63dbba71d3d11cfa
jordan <jordan.huang@gapp.nthu.edu.tw>
2020-09-10 14:50:38 +0800

    add tmux startup script

---
+++ starttmux.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Operating System(s):    Linux distros that use apt-get
+# Software Dependencies:  webserver ssh, htop, vim, git, ncdu, crawl, slurm
+# Description:            creates 10 named windows for general workday use
+
+TMUX_NAME="work"
+
+# check if session exists
+tmux has-session -t "$TMUX_NAME"
+if [ $? != 0 ]
+then
+
+    # if session does not exist create it
+    # note - default window numbered either 0 or 1 depending on tmux 'base-index'
+    # note - my setup assumes 'base-index' was set at 1
+    tmux -2 new-session -s "$TMUX_NAME" -n 福耀 -d
+    
+    
+    # create additional named windows
+    # note - extra windows start at 1 or 2 and count upward depending on tmux 'base-index'
+    # note - my setup assumes 'base-index' was set at 1
+    tmux new-window -n Ledger
+    tmux new-window -n JavaExample
+    
+    tmux send-keys -t "$TMUX_NAME":1 'cd ~/synergies/fu-glass/backend' C-m
+    tmux send-keys -t "$TMUX_NAME":2 'cd ~/Dropbox/Ledger' C-m
+    
+    
+    tmux send-keys -t "$TMUX_NAME":3 'cd ~/github/JavaExample/ ' C-m
+    tmux split-window -v -p 30 -t "$TMUX_NAME":3 
+
+fi
+tmux attach -t "$TMUX_NAME"
+
